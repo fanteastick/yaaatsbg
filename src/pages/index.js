b3d8652dbd1654e093c2d20dcde098c6c8f430bf
Tea <tea.riff.ick@gmail.com>
2018-11-10 19:52:41 -0800

    added link and made buildable

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,8 @@ const IndexPage = () => (
     <h1>Hi people what is Eilleen</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <a href="https://medium.freecodecamp.org/freecodecamp-is-hosting-a-free-hackathon-at-github-in-san-francisco-and-an-online-hackathon-too-2078088df278">This is the original tutorial</a>
+
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
